db832bde3e3a603a9fe1662a8e2f2dc65c9744e6
mstauff <mstauff>
2012-08-21 15:42:14 -0600

    Updated UI from old app to callings

---
+++ src/org/lds/community/CallingWorkFlow/CallingListActivity.java
@@ -0,0 +1,60 @@
+package org.lds.community.CallingWorkFlow;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import org.lds.community.CallingWorkFlow.domain.WorkFlowDB;
+
+public class CallingListActivity extends Activity implements AdapterView.OnItemSelectedListener
+{
+    WorkFlowDB db;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Log.i( "CallingListActivity", "in create" );
+        setContentView(R.layout.main);
+        Spinner callingGroupsSpinner = (Spinner) findViewById( R.id.callingGroupsSpinner);
+        callingGroupsSpinner.setOnItemSelectedListener(this);
+        db = new WorkFlowDB( getApplicationContext() );
+        Log.i( "CallingListActivity", "exit create" );
+    }
+
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        if( parent.getId() == R.id.callingGroupsSpinner ) {
+            // lookup new class
+//            ListView classMembersList = (ListView) findViewById( R.id.classMembersList );
+            Toast infoToast = Toast.makeText( getApplicationContext(), "Changed to " + parent.getSelectedItem().toString(), Toast.LENGTH_SHORT );
+            infoToast.show();
+
+        }
+
+    }
+
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void updateCallingList(View view) {
+        Spinner callingGroupsSpinner = (Spinner) findViewById( R.id.callingGroupsSpinner );
+        String className = (String) callingGroupsSpinner.getSelectedItem();
+
+    }
+
+    public void addNewCalling(View view) {
+        Toast infoToast = Toast.makeText( getApplicationContext(), "You clicked the + button", Toast.LENGTH_SHORT );
+        infoToast.show();
+
+    }
+    public void removeCalling(View view) {
+        Toast infoToast = Toast.makeText( getApplicationContext(), "You clicked the - button", Toast.LENGTH_SHORT );
+        infoToast.show();
+
+    }
+}
