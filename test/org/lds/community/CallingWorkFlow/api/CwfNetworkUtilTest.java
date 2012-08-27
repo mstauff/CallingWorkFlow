c85c9b704a245422053f109a4c2ef7dad270495e
Alex Carrasco <alexc@reddibooks.com>
2012-08-27 14:50:51 -0600

    New testing classes

---
+++ test/org/lds/community/CallingWorkFlow/api/CwfNetworkUtilTest.java
@@ -0,0 +1,42 @@
+package org.lds.community.CallingWorkFlow.api;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AlexC
+ * Date: 8/27/12
+ * Time: 2:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CwfNetworkUtilTest {
+    @Test
+    public void testGetHttpClient() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWardList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPositionIds() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStatuses() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPendingCallings() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCompletedCallings() throws Exception {
+
+    }
+}
---
+++ test/org/lds/community/CallingWorkFlow/domain/WorkFlowDBTest.java
@@ -0,0 +1,54 @@
+package org.lds.community.CallingWorkFlow.domain;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AlexC
+ * Date: 8/27/12
+ * Time: 2:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WorkFlowDBTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveCallings() throws Exception {
+
+    }
+
+    @Test
+    public void testSavePositions() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCallingIds() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPendingCallings() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCompletedCallings() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWardList() throws Exception {
+
+    }
+}
