7b559c6094207500ab8b3ae460ccfe065a9ec859
blanchardb <bblanchard12@yahoo.com>
2012-09-05 11:09:44 -0600

    Added list fragment to the UI for showing callings.  Incomplete database values and binding.  Needs to be bound.

--- src/main/java/org/lds/community/CallingWorkFlow/activity/CallingListFragment.java
+++ src/main/java/org/lds/community/CallingWorkFlow/activity/CallingListFragment.java
@@ -4,19 +4,21 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.CursorAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import org.lds.community.CallingWorkFlow.R;
 import org.lds.community.CallingWorkFlow.api.CwfNetworkUtil;
 import org.lds.community.CallingWorkFlow.domain.WorkFlowDB;
-import org.lds.community.CallingWorkFlow.wigdets.robosherlock.fragment.RoboSherlockFragment;
+import roboguice.fragment.RoboListFragment;
 
 import javax.inject.Inject;
 
-public class CallingListFragment  extends RoboSherlockFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class CallingListFragment extends RoboListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
     WorkFlowDB db;
 
     @Inject
@@ -24,29 +26,33 @@ public class CallingListFragment  extends RoboSherlockFragment implements Loader
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
     }
-    public void onNothingSelected(AdapterView<?> adapterView) {
-    }
-    public void updateCallingList(View view) {
-    }
-    public void addNewCalling(View view) {
-    }
-    public void removeCalling(View view) {
-    }
     private int currentPositionInList = 0;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(false);
-    }
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		"Linux", "OS/2" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+		android.R.layout.simple_list_item_1, values);
+		setListAdapter(adapter);
+
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+	// Do something with the data
+
+	}
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 	    return getLayoutInflater( savedInstanceState ).inflate( R.layout.callingworkflow_list, container );
-        //return inflater.inflate(R.layout.callingworkflow_list, container, false);
     }
-
+	/*
 	@Override
 	public void onPause() {
 		super.onPause();
@@ -61,7 +67,7 @@ public class CallingListFragment  extends RoboSherlockFragment implements Loader
     public void selectPosition(int position, long id) {
         currentPositionInList = position;
     }
-
+    */
     private CursorAdapter listAdapter;
 
 	@Override
--- src/main/java/org/lds/community/CallingWorkFlow/activity/WorkFlowActivity.java
+++ src/main/java/org/lds/community/CallingWorkFlow/activity/WorkFlowActivity.java
@@ -2,8 +2,9 @@ package org.lds.community.CallingWorkFlow.activity;
 
 import android.os.Bundle;
 import org.lds.community.CallingWorkFlow.R;
+import org.lds.community.CallingWorkFlow.wigdets.robosherlock.activity.RoboSherlockFragmentActivity;
 
-public class WorkFlowActivity extends org.lds.community.CallingWorkFlow.wigdets.robosherlock.activity.RoboSherlockFragmentActivity {
+public class WorkFlowActivity extends RoboSherlockFragmentActivity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
