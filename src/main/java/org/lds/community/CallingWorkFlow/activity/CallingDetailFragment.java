658008a2f19696a1044a236103aaba5a59f2226e
Adam <ashepherd@ldschurch.org>
2012-09-12 10:10:07 -0600

    Calling Detail fragment UI improvement and database integration.

--- src/main/java/org/lds/community/CallingWorkFlow/activity/CallingDetailFragment.java
+++ src/main/java/org/lds/community/CallingWorkFlow/activity/CallingDetailFragment.java
@@ -2,18 +2,83 @@ package org.lds.community.CallingWorkFlow.activity;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import org.lds.community.CallingWorkFlow.R;
+import org.lds.community.CallingWorkFlow.domain.Member;
+import org.lds.community.CallingWorkFlow.domain.Position;
+import org.lds.community.CallingWorkFlow.domain.WorkFlowDB;
 import org.lds.community.CallingWorkFlow.wigdets.robosherlock.fragment.RoboSherlockFragment;
+import roboguice.inject.InjectView;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CallingDetailFragment extends RoboSherlockFragment {
+    @Inject
+    WorkFlowDB db;
+    @InjectView(value = R.id.saveCallingButton)
+    Button saveCallingButton;
+    @InjectView(value = R.id.cancelButton)
+    Button cancelButton;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        saveCallingButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                saveCalling(view);
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                cancelChanges(view);
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return getLayoutInflater( savedInstanceState ).inflate( R.layout.callingdetail_fragment, container );
+        View view = getLayoutInflater( savedInstanceState ).inflate( R.layout.callingdetail_fragment, container );
+
+        AutoCompleteTextView callingField = (AutoCompleteTextView) view.findViewById(R.id.callingPosition);
+        List<Position> positions = db.getPositions();
+        List<String> positionNames = new ArrayList<String>();
+        for(Position v: positions){
+            positionNames.add(v.getPositionName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.autocomplete_textview,positionNames);
+        callingField.setAdapter(adapter);
+        callingField.setThreshold(1);//Number of required characters for search
+
+        AutoCompleteTextView memberField = (AutoCompleteTextView) view.findViewById(R.id.memberName);
+        List<Member> members = db.getWardList();
+        List<String> memberNames = new ArrayList<String>();
+        for(Member m: members){
+            memberNames.add(m.getFirstName() + " " + m.getLastName());
+        }
+        ArrayAdapter<String> memberAdapter = new ArrayAdapter<String>(getActivity(),R.layout.autocomplete_textview,memberNames);
+        memberField.setAdapter(memberAdapter);
+        memberField.setThreshold(1);//Number of required characters for search
+
+        return view;
+    }
+
+    public void saveCalling(View v){
+
+    }
+
+    public void cancelChanges(View v){
+
     }
 
 }
