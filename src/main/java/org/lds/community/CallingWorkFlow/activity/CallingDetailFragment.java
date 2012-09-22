276f47f65dadae228ef38b998a1187188e571cf4
Adam <ashepherd@ldschurch.org>
2012-09-22 16:27:04 -0600

    Calling Detail Save button and edit calling fix

--- src/main/java/org/lds/community/CallingWorkFlow/activity/CallingDetailFragment.java
+++ src/main/java/org/lds/community/CallingWorkFlow/activity/CallingDetailFragment.java
@@ -29,11 +29,26 @@ public class CallingDetailFragment extends RoboSherlockFragment {
     AutoCompleteTextView positionField;
     AutoCompleteTextView memberField;
     Spinner statusSpinner;
-
+    CallingViewItem callingViewItem;
+    Position selectedPosition;
+    Member selectedMember;
+    List<WorkFlowStatus> statusList;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+        Bundle bundle = getActivity().getIntent().getExtras();
+        if(bundle != null){
+            callingViewItem = (CallingViewItem)bundle.get("callingViewItems");
+            positionField.setText(callingViewItem.getPositionName());
+            memberField.setText(callingViewItem.getFullName());
+            for(WorkFlowStatus s: statusList){
+                if(callingViewItem.getStatusName().equals(s.getStatusName())){
+                    statusSpinner.setSelection(statusList.indexOf(s));
+                    break;
+                }
+            }
+        }
         saveCallingButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -57,16 +72,30 @@ public class CallingDetailFragment extends RoboSherlockFragment {
         List<? extends Listable> positionList = db.getPositions();
         ObjectListAdapter positionAdapter = new ObjectListAdapter(getActivity(),R.layout.autocomplete_textview,positionList);
         positionField.setAdapter(positionAdapter);
-        positionField.setThreshold(1);//Number of required characters for search
+        positionField.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        selectedPosition = (Position) positionField.getAdapter().getItem(i);
+                    }
+                }
+        );
 
         memberField = (AutoCompleteTextView) view.findViewById(R.id.memberName);
         List<? extends Listable> memberList = db.getWardList();
         ObjectListAdapter memberAdapter = new ObjectListAdapter(getActivity(),R.layout.autocomplete_textview,memberList);
         memberField.setAdapter(memberAdapter);
-        memberField.setThreshold(1);//Number of required characters for search
+        memberField.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        selectedMember = (Member) memberField.getAdapter().getItem(i);
+                    }
+                }
+        );
 
         statusSpinner = (Spinner) view.findViewById(R.id.status_spinner);
-        List<WorkFlowStatus> statusList = db.getWorkFlowStatuses();
+        statusList = db.getWorkFlowStatuses();
         List<String> statusOptions = new ArrayList<String>();
         for(WorkFlowStatus s: statusList) { statusOptions.add(s.getStatusName()); }
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,statusOptions);
@@ -77,13 +106,19 @@ public class CallingDetailFragment extends RoboSherlockFragment {
     }
 
     public void saveCalling(View v){
-        Long memberId = ((Member)memberField.getAdapter().getItem(memberField.getListSelection())).getIndividualId();
-        Long positionId = ((Position)positionField.getAdapter().getItem(positionField.getListSelection())).getPositionId();
-        String statusName = (String) statusSpinner.getSelectedItem();
-        Log.i("temp","***"+positionId+" "+memberId+" "+statusName);
-        //Calling calling = new Calling();
-        //calling
-        //callingManager.saveCalling(calling);
+        if(selectedPosition != null && selectedMember != null){
+            String statusName = (String) statusSpinner.getSelectedItem();
+            Long positionId = selectedPosition.getPositionId();
+            Long memberId = selectedMember.getIndividualId();
+            Calling calling = new Calling();
+            calling.setPositionId(memberId);
+            calling.setIndividualId(positionId);
+            calling.setStatusName(statusName);
+            callingManager.saveCalling(calling,getActivity());
+            Log.i("Detail Page","Calling Saved:"+selectedMember.getDisplayString()+
+                    ", "+selectedPosition.getDisplayString()+", "+statusName);
+        }
+
     }
 
     public void cancelChanges(View v){
